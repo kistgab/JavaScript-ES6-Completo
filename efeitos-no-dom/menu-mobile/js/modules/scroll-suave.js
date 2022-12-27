dd13ed9861cc2ae5dfabdc744aa323548fad41de
Gabriel Kist <gabrielkist02@gmail.com>
2022-12-26 23:24:34 -0300

    Iniciando menu-mobile e ajustando erro dropdown-menu

---
+++ efeitos-no-dom/menu-mobile/js/modules/scroll-suave.js
@@ -0,0 +1,19 @@
+export default function initSmoothScroll() {
+  const linksInternos = document.querySelectorAll("[data-menu='suave'] a[href^='#']");
+  linksInternos.forEach((link) => link.addEventListener("click", scrollToSection));
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+    });
+
+    // forma alternativa
+    // window.scrollTo({
+    //   top: section.offsetTop,
+    //   behavior: "smooth",
+    // });
+  }
+}
