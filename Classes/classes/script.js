3f5b4f4986daf894a9756c4c5506a20cdf99d8e6
Gabriel Kist <gabrielkist02@gmail.com>
2022-12-29 19:42:46 -0300

    classes

---
+++ Classes/classes/script.js
@@ -0,0 +1,46 @@
+// function Button(text, background) {
+//   this.text = text;
+//   this.background = background;
+// }
+
+// Button.prototype.element = function () {
+//   const buttonElement = document.createElement("button");
+//   buttonElement.innerText = this.text;
+//   buttonElement.style.background = this.background;
+//   return buttonElement;
+// };
+
+// const blueButton = new Button("Comprar", "blue");
+// console.log(blueButton.element());
+
+class Button {
+  constructor(text, background) {
+    this.text = text;
+    this.background = background;
+  }
+  element() {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = this.text;
+    buttonElement.style.background = this.background;
+    return buttonElement;
+  }
+
+  appendTo(target) {
+    const targetElement = document.querySelector(target);
+    targetElement.appendChild(this.element());
+    return targetElement;
+  }
+
+  // If i Want a constructor Method, i can use the "static" keyword in front of the method, so the only way to access it will be Button.method()
+  static createButton(text, background) {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = text;
+    buttonElement.style.background = background;
+    return buttonElement;
+  }
+}
+
+const blueButton = new Button("Comprar", "blue");
+console.log(blueButton.appendTo("body"));
+
+const blueButtonStatic = Button.createButton("comprar", blue);
